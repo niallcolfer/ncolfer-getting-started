253df5e972f5f8e46314d6114c811879f1b70dd4
Niall Colfer <ncolfer@7028-ncolfer-367.local>
2016-12-21 17:02:04 +0000

    add POST listener

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+});
+
+app.post('/', function(req, res) {
+	res.send('POST request');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
