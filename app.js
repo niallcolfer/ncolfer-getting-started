298801d5ec03035abbb28b93f920698324853496
Niall Colfer <ncolfer@7028-ncolfer-367.local>
2016-12-21 16:28:01 +0000

    update port on listen

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(app.get('port'), function () {
-  console.log('Example app listening on port', app.get('port'));
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
